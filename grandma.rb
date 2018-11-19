654b5df3603be42aaaefc6b3709d49cd3f7223d2
DANIELLE PARK <dpark1008@gmail.com>
2018-11-19 04:45:45 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -7,9 +7,7 @@ def speak_to_grandma(phrase)
 end
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif (phrase=="#{phrase}".upcase)
-    return "NO, NOT SINCE 1938!"
-  else
+  elsif (phrase == phrase.downcase)
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
