400ac8c37bdc1fd8be68f1bf8c98be4bcdf81a8d
DANIELLE PARK <dpark1008@gmail.com>
2018-11-19 04:46:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
   else
   return "HUH?! SPEAK UP, SONNY!"
 end
-  if phrase=="I LOVE YOU GRANDMA!"
+  if (phrase=="I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
   elsif (phrase==phrase.downcase)
     return "HUH?! SPEAK UP, SONNY!"
